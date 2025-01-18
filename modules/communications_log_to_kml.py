bf289e42a95a0762892c10809c0f70fcdc0ed59b
Evan Zhao <57083990+Evang264@users.noreply.github.com>
2025-01-18 02:26:35 -0500

    Communications log converter: convert last line
    On top of converting the entire log file to KML, convert the last line
    of the log file to KML too.

--- modules/communications_log_to_kml.py
+++ modules/communications_log_to_kml.py
@@ -10,60 +10,91 @@ from .common.modules.kml import kml_conversion
 from .common.modules import location_global
 
 
-DEFAULT_RESULTS_PATH = pathlib.Path("results")
-
-
-def convert_communication_log_to_kml(
-    log_file_path: pathlib.Path, document_name_prefix: str, save_directory: pathlib.Path
-) -> "tuple[bool, pathlib.Path | None]":
+def _convert_communication_log_to_kml(
+    log_file: str, document_name_prefix: str, save_directory: pathlib.Path
+) -> bool:
     """
-    Given a communications log file with a specific format, return a corresponding KML file.
+    Given a communications log file, write it to a KML file.
 
     Args:
         log_file (str): Path to the communications log file
         document_name_prefix (str): Prefix name for saved KML file.
-        save_directory (str): Directory to save the KML file to.
+        save_directory (pathlib.Path): Directory to save the KML file to.
 
     Returns:
-        tuple[bool, pathlib.Path | None]: Returns (False, None) if function
-            failed to execute, otherwise (True, path) where path a pathlib.Path
-            object pointing to the KML file.
+        bool: True on success, False otherwise.
     """
     locations = []
 
-    with open(log_file_path, "r", encoding="utf-8") as log_file:
-        for line in log_file:
-            # Use the timestamp (hh:mm:ss) as the name of the marker in the KML file
-            time = line[:8]
-            # find all the latitudes and longitudes within the line
-            latitudes = re.findall(r"latitude: (-?\d+\.\d+)", line)
-            longitudes = re.findall(r"longitude: (-?\d+\.\d+)", line)
+    for line in log_file.split("\n"):
+        # Use the timestamp (hh:mm:ss) as the name of the marker in the KML file
+        time = line[:8]
+        # find all the latitudes and longitudes within the line
+        latitudes = re.findall(r"latitude: (-?\d+\.\d+)", line)
+        longitudes = re.findall(r"longitude: (-?\d+\.\d+)", line)
 
-            # we must find equal number of latitude and longitude numbers,
-            # otherwise that means the log file is improperly formatted or
-            # the script failed to detect all locations
-            if len(latitudes) != len(longitudes):
-                print("Number of latitudes and longitudes found are different.")
-                print(f"# of altitudes: {len(latitudes)}, # of longitudes: {len(longitudes)}")
+        # we must find equal number of latitude and longitude numbers,
+        # otherwise that means the log file is improperly formatted or
+        # the script failed to detect all locations
+        if len(latitudes) != len(longitudes):
+            print("Number of latitudes and longitudes found are different.")
+            print(f"# of altitudes: {len(latitudes)}, # of longitudes: {len(longitudes)}")
+            return False, None
+
+        # Convert list of strings to list of floats
+        latitudes = list(map(float, latitudes))
+        longitudes = list(map(float, longitudes))
+
+        # Cannot use for each loop here, iterating through both lists at the same time
+        # pylint: disable-next=consider-using-enumerate
+        for i in range(len(latitudes)):
+            result, location = location_global.NamedLocationGlobal.create(
+                f"{time} {i}", latitudes[i], longitudes[i]
+            )
+            if not result:
+                print(f"Failed to create location for time {time} and index {i}.")
                 return False, None
 
-            # Convert list of strings to list of floats
-            latitudes = list(map(float, latitudes))
-            longitudes = list(map(float, longitudes))
+            locations.append(location)
 
-            # Cannot use for each loop here, iterating through both lists at the same time
-            # pylint: disable-next=consider-using-enumerate
-            for i in range(len(latitudes)):
-                result, location = location_global.NamedLocationGlobal.create(
-                    time + " " + i, latitudes[i], longitudes[i]
-                )
-                if not result:
-                    print(f"Failed to create location for time {time} and index {i}.")
-                    return False, None
+    result, path = kml_conversion.named_locations_to_kml(
+        locations, document_name_prefix, save_directory
+    )
+    return result
 
-                locations.append(location)
 
-    return kml_conversion.named_locations_to_kml(locations, document_name_prefix, save_directory)
+def convert_communication_log_to_kml(
+    log_file_path: pathlib.Path, document_name_prefix: str, save_directory: pathlib.Path
+) -> bool:
+    """
+    Given a communications log file, create a KML of the entire file and a KML
+    for the last line.
+
+    Args:
+        log_file_path (pathlib.Path): Path to the communications log file
+        document_name_prefix (str): Prefix name for saved KML files.
+        save_directory (str): Directory to save the KML files to.
+
+    Returns:
+        bool: True on success, False otherwise.
+    """
+    with open(log_file_path, "r", encoding="utf-8") as log_file:
+        content = log_file.read().strip()
+
+        # Convert the entire log file to KML file
+        result = _convert_communication_log_to_kml(content, document_name_prefix, save_directory)
+        if not result:
+            print(f"Failed to convert {log_file_path} to KML.")
+            return False
+
+        # Convert only the last line to KML file
+        result = _convert_communication_log_to_kml(
+            content.split("\n")[-1], document_name_prefix + "_last_line", save_directory
+        )
+        if not result:
+            print(f"Failed to convert {log_file_path} to KML.")
+            return False
+    return True
 
 
 # similar main to other logs to kml scripts
@@ -73,9 +104,8 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    DEFAULT_RESULTS_PATH.mkdir(exist_ok=True, parents=True)
-    conversion_result, path = convert_communication_log_to_kml(
-        str(DEFAULT_RESULTS_PATH), "", args.output_dir
+    conversion_result = convert_communication_log_to_kml(
+        args.log_path, "output", pathlib.Path.cwd()
     )
     if conversion_result:
         print("Done!")
